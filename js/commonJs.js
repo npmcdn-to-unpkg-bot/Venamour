2dddac6b3ada16f22b451a91032f92be4ac34da2
Zhe1ka <zheka.dzecina@gmail.com>
2016-02-11 22:00:14 +0200

    Zhe1ka

--- js/commonJs.js
+++ js/commonJs.js
@@ -15,8 +15,32 @@ $(document).ready(function(){
 
     setTimeout(function(){
         $('.grid').masonry({
-            // options
             itemSelector: '.grid-item'
         });
     }, 200);
+
+    function scrollMenu(){
+        var scrollHeight = $(document).scrollTop() + 75,
+            boxMenu = $('#page-header'),
+            windowH = $(window).height();
+        console.log('111')
+
+        if(scrollHeight > (windowH - 1) && !boxMenu.hasClass('pos-menu')) {
+            boxMenu.addClass('pos-menu')
+        }
+        else if (scrollHeight < windowH) {
+            if(boxMenu.hasClass('pos-menu')) {
+                boxMenu.removeClass('pos-menu');
+            }
+        }
+    }
+
+
+    $(document).scroll(function(){
+        scrollMenu();
+    });
+
+    $(document).resize(function(){
+        scrollMenu();
+    });
 });
