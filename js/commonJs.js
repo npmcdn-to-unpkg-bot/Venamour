6fa130dd09d9128ad10093711363b1d3456aca2f
Zhe1ka <zheka.dzecina@gmail.com>
2016-02-11 19:56:45 +0200

    Zhe1ka

--- js/commonJs.js
+++ js/commonJs.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
+    $('#continue').on('click', function(e){
+        e.preventDefault();
 
+        $('#expanded').show();
+    });
+
+    $('#btn-open-menu').on('click', function(){
+        $('#main-nav').addClass('is-open');
+    });
+
+    $('#close-button').on('click', function(){
+        $('#main-nav').removeClass('is-open');
+    });
 });
